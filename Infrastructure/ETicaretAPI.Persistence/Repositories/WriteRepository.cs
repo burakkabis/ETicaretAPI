d299fb95d3c99500ed5bc7226c909c3b992e726e
burakkabis <134310460+burakkabis@users.noreply.github.com>
2024-02-23 16:44:11 +0300

    2/23/2024

--- Infrastructure/ETicaretAPI.Persistence/Repositories/WriteRepository.cs
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/WriteRepository.cs
@@ -21,7 +21,7 @@ namespace ETicaretAPI.Persistence.Repositories
             _context = context;
         }
 
-        public DbSet<T> Table => throw new NotImplementedException();
+        public DbSet<T> Table => _context.Set<T>();
 
         public async Task<bool> AddAsync(T model)
         {
--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ETicaretAPI.API.Controllers
@@ -7,5 +8,27 @@ namespace ETicaretAPI.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+
+        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        {
+            _productReadRepository = productReadRepository;
+            _productWriteRepository = productWriteRepository;
+        }
+        [HttpGet]
+        public async Task Get()
+        {
+            await _productWriteRepository.AddRangeAsync(new()
+            {
+                new() {Id=Guid.NewGuid(),Name="Product 1",Price=100,CreatedDate=DateTime.UtcNow,Stock=12 },
+                new() {Id=Guid.NewGuid(),Name="Product 2",Price=120,CreatedDate=DateTime.UtcNow,Stock=22 },
+                new() {Id=Guid.NewGuid(),Name="Product 3",Price=130,CreatedDate=DateTime.UtcNow,Stock=32 },
+                new() {Id=Guid.NewGuid(),Name="Product 4",Price=140,CreatedDate=DateTime.UtcNow,Stock=23 },
+
+            });
+            await _productWriteRepository.SaveAsync();
+        }
+
     }
 }
